b649dcfe7312fbc6dc10aba745df204f9fff949b
stevenanderson-git <sdeananderson@gmail.com>
2021-03-28 20:25:49 -0400

    javascript for filter list added

--- static/js/search.js
+++ static/js/search.js
@@ -56,3 +56,42 @@ function filter_sub(selected){
     let category_value = Number(selected.options[selected.selectedIndex].value);
     populate_sub(category_value);
 }
+// Add filters to list
+// TODO: add check to prevent multiple additions
+function add_to_filters(selected){
+    let sub_cat = selected.options[selected.selectedIndex];
+    let filter_list = document.getElementById("filter-list");
+    // Create clear all button if empty
+    if(filter_list.innerHTML===""){
+        let clearall = document.createElement("li");
+        let clearbutton = document.createElement("button");
+        clearbutton.type = "button";
+        clearbutton.value = "clear-all"
+        clearbutton.className = "filter-tag clear-all";
+        clearbutton.onclick = clear_all_filters();
+        clearbutton.innerHTML = "Clear All";
+        clearall.appendChild(clearbutton);
+        filter_list.append(clearall);
+    }
+
+    let filter = document.createElement("li");
+    filter.className = "filter-tag";
+    filter.value = sub_cat.value;
+    let filter_button = document.createElement("button");
+    filter_button.className = "filter-tag-button";
+    filter_button.type = "button";
+    filter_button.value = sub_cat.value;
+    filter_button.onclick = remove_from_filters();
+    filter_button.innerHTML = sub_cat.text + " <i class='fas fa-times'></i>";
+    filter.appendChild(filter_button);
+    filter_list.prepend(filter);
+}
+// Remove filters from list
+function remove_from_filters(){
+
+}
+// Remove all filters from list
+function clear_all_filters(){
+    let filter_list = document.getElementById("filter-list");
+    filter_list.length = 0;
+}
