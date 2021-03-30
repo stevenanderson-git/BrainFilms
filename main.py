50580080b8304fda9e58dd650349096f7147151c
Santino Celebre <santinocelebre@gmail.com>
2021-03-29 20:53:49 -0400

    Admin addition

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.secret_key = 'your secret key'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 # TODO: ask professor about proper passwords for website.
-app.config['MYSQL_PASSWORD'] = 'mysql' # change this back to 'root' if changed
+app.config['MYSQL_PASSWORD'] = 'root' # change this back to 'root' if changed
 # TODO: change database name for uniforimity in project.
 app.config['MYSQL_DB'] = 'braindb'
 
@@ -58,6 +58,36 @@ def login():
             msg = 'Incorrect username/password!'
     # Show the login form with message (if any)
     return render_template('login.html', title = title, msg=msg)
+
+#admin login page 
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    title = 'Admin'
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM UserInfo WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Go to Profile page
+            return profile(account)
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('admin.html', title = title, msg=msg)
 
 # TODO: Logout not currently used
 @app.route('/logout')
