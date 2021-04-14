ac606d6fcde61b132c42f0e2f6e175b3971bbb6e
stevenanderson-git <sdeananderson@gmail.com>
2021-04-14 17:45:36 -0400

    dynamic hide/show of divs for categories

--- static/js/categories.js
+++ static/js/categories.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
     $("#submit-button").on('click', function() {
         console.log('hi');
     });
+    $("#category").on('change', function(){
+        let selected = $("select[name=category] option").filter(':selected').val();
+        console.log(selected);
+        if( selected == 8){
+            $(".c2").show();
+        }
+        else{
+            $(".c2").hide();
+        }
+        
+    });
 });
